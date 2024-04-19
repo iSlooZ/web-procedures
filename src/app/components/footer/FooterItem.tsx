1ca2ed1377d58f3417c07277e63d2aa1996f5240
iSlooZ <michel.dev26@gmail.com>
2024-04-19 12:06:32 -0400

    react hook form terminado

--- src/app/components/footer/FooterItem.tsx
+++ src/app/components/footer/FooterItem.tsx
@@ -2,21 +2,13 @@
 
 export const FooterItem = () => {
   return (
-    <div className="w-1/3 flex flex-col justify-center items-start">
-      <h3 className="text-2xl font-medium py-4">Datos de Contacto</h3>
+    <div className="w-1/6 flex flex-col justify-start items-start">
+      <h3 className="text-xl text-slate-600 font-medium py-4">Recursos</h3>
       <div className="flex justify-center items-center">
-        <img className="w-10" src="/ig.svg" alt="" />
         <div>
-          <h4>Contacto@knowhow.com</h4>
-          <h4>Escribenos</h4>
-        </div>
-      </div>
-      <h3 className="text-xl font-medium">Datos de Contacto</h3>
-      <div className="flex">
-        <img className="w-10" src="/ig.svg" alt="" />
-        <div>
-          <h4>Contacto@knowhow.com</h4>
-          <h4>Escribenos</h4>
+          <h4>Como se usa</h4>
+          <h4>Como acceder</h4>
+          <h4>Precios</h4>
         </div>
       </div>
     </div>
--- src/app/components/shared/footer/Footer.tsx
+++ src/app/components/shared/footer/Footer.tsx
@@ -3,18 +3,18 @@ import { FooterItem } from "../../footer/FooterItem"
 
 export const Footer = () => {
   return (
-    <footer className="w-full flex justify-center items-center p-8 bg-[#f3f6fa]">
+    <footer className="w-full min-h-64 flex justify-center items-center p-8 bg-[#f3f6fa]">
       <div className="w-1/4 flex flex-col justify-center items-center">
         <img className="w-80 pb-4" src="/logotest.png" alt="logo test" />
         <ul className="flex justify-center items-center">
-          <li><a href=""><img className="w-10" src="/ig.svg" alt="" /></a></li>
-          <li><a href=""><img className="w-10" src="/ig.svg" alt="" /></a></li>
-          <li><a href=""><img className="w-10" src="/ig.svg" alt="" /></a></li>
-          <li><a href=""><img className="w-10" src="/ig.svg" alt="" /></a></li>
-          <li><a href=""><img className="w-10" src="/ig.svg" alt="" /></a></li>
+          <li><a href=""><img className="w-6" src="/ig.svg" alt="" /></a></li>
+          <li><a href=""><img className="w-6" src="/ig.svg" alt="" /></a></li>
+          <li><a href=""><img className="w-6" src="/ig.svg" alt="" /></a></li>
+          <li><a href=""><img className="w-6" src="/ig.svg" alt="" /></a></li>
+          <li><a href=""><img className="w-6" src="/ig.svg" alt="" /></a></li>
         </ul>
       </div>
-      <div className="w-3/4 flex justify-center items-center">
+      <div className="w-2/4 flex justify-start items-center">
         <FooterItem />
         <FooterItem />
         <FooterItem />
